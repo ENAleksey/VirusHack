20e141916c0ecd51a7250711b73382956371cd03
Maxsior <maxsior@mail.ru>
2020-05-03 17:19:08 +0500

    change path to model

--- test.py
+++ test.py
@@ -3,16 +3,12 @@ import sys
 import os
 import wave
 
-if not os.path.exists("model-ru"):
-    print ("Please download the model from https://github.com/alphacep/kaldi-android-demo/releases and unpack as 'model-en' in the current folder.")
-    exit (1)
-
 wf = wave.open(sys.argv[1], "rb")
 if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getcomptype() != "NONE":
     print ("Audio file must be WAV format mono PCM.")
     exit (1)
 
-model = Model("model-ru")
+model = Model("models/ru")
 rec = KaldiRecognizer(model, wf.getframerate())
 
 while True:
