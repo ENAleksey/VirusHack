2b79240c3c0029dc91574cd397b1058b3ca2f31c
ENAleksey <EN.Aleksey@gmail.com>
2020-05-03 22:15:23 +0500

    implemented hand tracking in rectangles

--- hand_tracking.py
+++ hand_tracking.py
@@ -3,55 +3,23 @@ import numpy as np
 import math
 
 
-cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
-    # read image
-    ret, img = cap.read()
-    img = cv2.flip(img, 1)
+def isHandInRect(frame, x, y, w, h):
+    crop_img = frame[y:y+h, x:x+w]
 
-    # get hand data from the rectangle sub window on the screen
-    cv2.rectangle(img, (200,200), (000,000), (0,255,0), 0)
-    crop_img = img[000:200, 000:200]
-
-    # convert to grayscale
     grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-
-    # applying gaussian blur
     blurred = cv2.GaussianBlur(grey, (35, 35), 0)
-
-    # thresholdin: Otsu's Binarization method
     _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
-    # show thresholded image
-    cv2.imshow('Thresholded', thresh1)
+    # cv2.imshow('Thresholded', thresh1)
 
     contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    # find contour with max area
     cnt = max(contours, key = lambda x: cv2.contourArea(x))
-
-    # create bounding rectangle around the contour (can skip below two lines)
-    x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(crop_img, (x, y), (x+w, y+h), (0, 0, 255), 0)
-
-    # finding convex hull
     hull = cv2.convexHull(cnt)
-
-    # drawing contours
-    drawing = np.zeros(crop_img.shape,np.uint8)
-    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 0)
-    cv2.drawContours(drawing, [hull], 0,(0, 0, 255), 0)
-
-    # finding convex hull
     hull = cv2.convexHull(cnt, returnPoints=False)
 
-    # finding convexity defects
     defects = cv2.convexityDefects(cnt, hull)
     count_defects = 0
-    cv2.drawContours(thresh1, contours, -1, (0, 255, 0), 3)
 
-    # applying Cosine Rule to find angle for all defects (between fingers)
-    # with angle > 90 degrees and ignore defects
     for i in range(defects.shape[0]):
         s,e,f,d = defects[i,0]
 
@@ -59,45 +27,43 @@ while(cap.isOpened()):
         end = tuple(cnt[e][0])
         far = tuple(cnt[f][0])
 
-        # find length of all sides of triangle
         a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
         b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
         c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
 
-        # apply cosine rule here
         angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
 
-        # ignore angles > 90 and highlight rest with red dots
         if angle <= 90:
             count_defects += 1
-            cv2.circle(crop_img, far, 1, [0,0,255], -1)
-        #dist = cv2.pointPolygonTest(cnt,far,True)
 
-        # draw a line from start to end i.e. the convex points (finger tips)
-        # (can skip this part)
-        cv2.line(crop_img,start, end, [0,255,0], 2)
-        #cv2.circle(crop_img,far,5,[0,0,255],-1)
+    return count_defects > 1
 
-    # define actions required
-    if count_defects > 1:
-        cv2.putText(img,"Hand detected", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+
+camera = cv2.VideoCapture(0)
+width = int(camera.get(3))
+height = int(camera.get(4))
+
+while(camera.isOpened()):
+    ret, frame = camera.read()
+    frame = cv2.flip(frame, 1)
+
+    # cv2.rectangle(frame, (200,200), (000,000), (0,255,0), 0)
+    # cv2.rectangle(frame, (width,height), (width-200,height-200), (0,255,0), 0)
+    # cv2.rectangle(frame, (200,height), (0,height-200), (0,255,0), 0)
+    # cv2.rectangle(frame, (width,200), (width-200,0), (0,255,0), 0)
+
+    if isHandInRect(frame, 0, 0, 200, 200):
+        cv2.putText(frame,"Top Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    elif isHandInRect(frame, width-200, 0, 200, 200):
+        cv2.putText(frame,"Top Right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    elif isHandInRect(frame, 0, height-200, 200, 200):
+        cv2.putText(frame,"Bottom Left", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+    elif isHandInRect(frame, width-200, height-200, 200, 200):
+        cv2.putText(frame,"Bottom Right", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
     else:
-        cv2.putText(img,"Nothing", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # if count_defects == 1:
-    #     cv2.putText(img,"2 fingers", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # elif count_defects == 2:
-    #     cv2.putText(img, "3 fingers", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # elif count_defects == 3:
-    #     cv2.putText(img,"4 fingers", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # elif count_defects == 4:
-    #     cv2.putText(img,"5 fingers", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
-    # else:
-    #     cv2.putText(img,"Nothing", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+        cv2.putText(frame,"Nothing", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
 
-    # show appropriate images in windows
-    cv2.imshow('Main', img)
-    all_img = np.hstack((drawing, crop_img))
-    cv2.imshow('Contours', all_img)
+    cv2.imshow('Main', frame)
 
     k = cv2.waitKey(10)
     if k == 27:
