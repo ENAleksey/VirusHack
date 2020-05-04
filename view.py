858de848dd4064627c280ddf34b8512d439a4f72
Maxsior <maxsior@mail.ru>
2020-05-04 20:26:37 +0500

    картинки для экранов

--- view.py
+++ view.py
@@ -1,35 +1,79 @@
+import cv2
 import commands
+from threading import Thread
 
 
 class Screen:
-    def __init__(self, name, commands=None):
+    def __init__(self, name, image, commands=None):
         if commands is None:
             commands = []
+
         self.name = name
+        self.image = image
         self.commands = commands
 
 
-approveScreen = Screen('Подтверждение ассистентом')
+approveScreen = Screen(
+    'Подтверждение ассистентом',
+    'screens/10.jpg'
+)
 
-cancelScreen = Screen('Отмена покупки', [
-    (commands.cancel, approveScreen),
-])
+cancelScreen = Screen(
+    'Отмена покупки',
+    '',
+    [
+        (commands.cancel, approveScreen),
+    ]
+)
 
-addItemScreen = Screen('Добавление товаров', [
-    (commands.addItem, None),
-    (commands.cancel, cancelScreen),
-])
+addItemScreen = Screen(
+    'Добавление товаров',
+    'screens/3.jpg',
+    [
+        (commands.addItem, None),
+        (commands.cancel, cancelScreen)
+    ]
+)
 
-startScreen = Screen('Начальный экран', [
-    (commands.start, addItemScreen)
-])
+startScreen = Screen(
+    'Начальный экран',
+    'screens/1.jpg',
+    [
+        (commands.start, addItemScreen)
+    ]
+)
 
 
 class View:
     def __init__(self):
         self.screen = startScreen
+        self.im = cv2.imread(self.screen.image)
+        self._loop = True
+        self._changed = True
+        Thread(target=self._show_image).start()
+
+    def _show_image(self):
+        while self._loop:
+            if self._changed:
+                self.im = cv2.imread(self.screen.image)
+                self._changed = False
+            cv2.imshow('view', self.im)
+            cv2.waitKey(1000)
 
     def set_screen(self, screen):
         if screen is not None:
             self.screen = screen
+            self._changed = True
             print('Screen changed to "{}"'.format(screen.name))
+
+    def destroy(self):
+        self._loop = False
+
+
+if __name__ == '__main__':
+    from time import sleep
+    view = View()
+    sleep(2)
+    view.set_screen(addItemScreen)
+    sleep(2)
+    view.destroy()
