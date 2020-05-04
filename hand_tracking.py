1da70707d39355ec687326ea8272ae3205d1000f
ENAleksey <EN.Aleksey@gmail.com>
2020-05-04 21:59:37 +0500

    code cleanup

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,70 +1,56 @@
 import cv2
-import numpy as np
 import math
 
 
+class HandTracker:
+    def __init__(self, maxDefects):
+        self.maxDefects = maxDefects
+    
+    def isHandInRect(self, frame, x, y, w, h):
+        crop_img = frame[y:y+h, x:x+w]
 
-learningRate = 0
+        grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(grey, (35, 35), 0)
+        _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
+        cv2.imshow(str("{} {} {} {}").format(x, y, w, h), thresh1)
 
+        contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        # contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        cnt = max(contours, key = lambda x: cv2.contourArea(x))
+        hull = cv2.convexHull(cnt)
+        hull = cv2.convexHull(cnt, returnPoints=False)
 
-def isHandInRect(frame, x, y, w, h):
-    crop_img = frame[y:y+h, x:x+w]
+        defects = cv2.convexityDefects(cnt, hull)
+        count_defects = 0
 
-    grey = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(grey, (35, 35), 0)
-    _, thresh1 = cv2.threshold(blurred, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        if defects is not None:
+            for i in range(defects.shape[0]):
+                s,e,f,d = defects[i,0]
 
-    cv2.imshow(str("{} {} {} {}").format(x, y, w, h), thresh1)
+                start = tuple(cnt[s][0])
+                end = tuple(cnt[e][0])
+                far = tuple(cnt[f][0])
 
-    contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # contours, hierarchy = cv2.findContours(thresh1.copy(),cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
-    hull = cv2.convexHull(cnt)
-    hull = cv2.convexHull(cnt, returnPoints=False)
+                a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+                b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+                c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
 
-    defects = cv2.convexityDefects(cnt, hull)
-    count_defects = 0
+                angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
 
-    if defects is not None:
-        for i in range(defects.shape[0]):
-            s,e,f,d = defects[i,0]
+                if angle <= 90:
+                    count_defects += 1
 
-            start = tuple(cnt[s][0])
-            end = tuple(cnt[e][0])
-            far = tuple(cnt[f][0])
+        return count_defects > self.maxDefects
 
-            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
-            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
-            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
-
-            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
-
-            if angle <= 90:
-                count_defects += 1
-
-    return count_defects > 1
-
-
-def getRectIdFromHandPos(frame, w, h):
-    if isHandInRect(frame, 0, 0, 200, 200):
-        return 0
-    elif isHandInRect(frame, w-200, 0, 200, 200):
-        return 1
-    elif isHandInRect(frame, 0, h-200, 200, 200):
-        return 2
-    elif isHandInRect(frame, w-200, h-200, 200, 200):
-        return 3
-    else:
-        return None
-
-
-def removeBG(frame):
-    fgmask = bgModel.apply(frame,learningRate=learningRate)
-    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    # res = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
-
-    kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    res = cv2.bitwise_and(frame, frame, mask=fgmask)
-    return res
+    def getRectIdFromHandPos(self, frame, w, h):
+        if self.isHandInRect(frame, 0, 0, 200, 200):
+            return 0
+        elif self.isHandInRect(frame, w-200, 0, 200, 200):
+            return 1
+        elif self.isHandInRect(frame, 0, h-200, 200, 200):
+            return 2
+        elif self.isHandInRect(frame, w-200, h-200, 200, 200):
+            return 3
+        else:
+            return None
--- test_hand.py
+++ test_hand.py
@@ -1,89 +1,87 @@
-from hand_tracking import getRectIdFromHandPos 
+from hand_tracking import HandTracker
 import cv2
 import commands
 from view import View
 import time
 
+
+view = View()
+handTracker = HandTracker(1)
 # camera = cv2.VideoCapture("http://192.168.100.12:8080/video")
 camera = cv2.VideoCapture(0)
 width = int(camera.get(3))
 height = int(camera.get(4))
-view = View()
-
 prevRectId = None
 currTime = None
 endTime = None
 
-isBgCaptured = 0
-bgSubThreshold = 50
-j=0
 
-
-print("\nТекущий экран: " + view.screen.name)
-for cmd, transition in view.screen.commands:
-    print(cmd.action + " " + transition.name if transition else "None")
-
-while(camera.isOpened()):
-    ret, frame = camera.read()
-    frame = cv2.flip(frame, 1)
-    frame_out = frame
-
-    # frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    # if(j==0):
-    #     bg=frame.copy().astype("float")
-    # if(j<30):
-    #     cv2.accumulateWeighted(frame,bg,0.5)
-    #     j=j+1
-    # frame=cv2.absdiff(frame,bg.astype("uint8"))
-    
-    # thre,frame=cv2.threshold(frame,25,255,cv2.THRESH_BINARY)
-
-    # if isBgCaptured == 1:
-        # frame = removeBG(frame)
-
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
+def renderDebugView(rectId):
     screens = len(view.screen.commands)
     colorRed = (0,0,255)
     colorGreen = (0,255,0)
 
     if screens == 1:
-        cv2.rectangle(frame_out, (200,200), (000,000), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,200), (width-200,0), colorRed, 0)
-        cv2.rectangle(frame_out, (200,height), (0,height-200), colorRed, 0)
-        cv2.rectangle(frame_out, (width,height), (width-200,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,200), (000,000), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,200), (width-200,0), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,height), (0,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (width,height), (width-200,height-200), colorRed, 0)
     elif screens == 2:
-        cv2.rectangle(frame_out, (200,200), (000,000), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,200), (width-200,0), colorGreen, 2)
-        cv2.rectangle(frame_out, (200,height), (0,height-200), colorRed, 0)
-        cv2.rectangle(frame_out, (width,height), (width-200,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,200), (000,000), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,200), (width-200,0), colorGreen, 2)
+        cv2.rectangle(camera_debug, (200,height), (0,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (width,height), (width-200,height-200), colorRed, 0)
     elif screens == 2:
-        cv2.rectangle(frame_out, (200,200), (000,000), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,200), (width-200,0), colorGreen, 2)
-        cv2.rectangle(frame_out, (200,height), (0,height-200), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,height), (width-200,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,200), (000,000), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,200), (width-200,0), colorGreen, 2)
+        cv2.rectangle(camera_debug, (200,height), (0,height-200), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,height), (width-200,height-200), colorRed, 0)
     elif screens == 2:
-        cv2.rectangle(frame_out, (200,200), (000,000), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,200), (width-200,0), colorGreen, 2)
-        cv2.rectangle(frame_out, (200,height), (0,height-200), colorGreen, 2)
-        cv2.rectangle(frame_out, (width,height), (width-200,height-200), colorGreen, 2)
+        cv2.rectangle(camera_debug, (200,200), (000,000), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,200), (width-200,0), colorGreen, 2)
+        cv2.rectangle(camera_debug, (200,height), (0,height-200), colorGreen, 2)
+        cv2.rectangle(camera_debug, (width,height), (width-200,height-200), colorGreen, 2)
     else:
-        cv2.rectangle(frame_out, (200,200), (000,000), colorRed, 0)
-        cv2.rectangle(frame_out, (width,200), (width-200,0), colorRed, 0)
-        cv2.rectangle(frame_out, (200,height), (0,height-200), colorRed, 0)
-        cv2.rectangle(frame_out, (width,height), (width-200,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,200), (000,000), colorRed, 0)
+        cv2.rectangle(camera_debug, (width,200), (width-200,0), colorRed, 0)
+        cv2.rectangle(camera_debug, (200,height), (0,height-200), colorRed, 0)
+        cv2.rectangle(camera_debug, (width,height), (width-200,height-200), colorRed, 0)
+
+    if rectId == 0:
+        cv2.putText(camera_debug, "Top Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+    elif rectId == 1:
+        cv2.putText(camera_debug, "Top Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+    elif rectId == 2:
+        cv2.putText(camera_debug, "Bottom Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+    elif rectId == 3:
+        cv2.putText(camera_debug, "Bottom Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+    else:
+        cv2.putText(camera_debug, "Nothing", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+
+    cv2.imshow('Camera', camera_debug)
 
 
-    rectId = getRectIdFromHandPos(frame, width, height)
+print("\nТекущий экран: " + view.screen.name)
+for cmd, transition in view.screen.commands:
+    print(cmd.action + " - " + transition.name if transition else "None")
+
+while(camera.isOpened()):
+    ret, frame = camera.read()
+    frame = cv2.flip(frame, 1)
+    camera_debug = frame
+
+    rectId = handTracker.getRectIdFromHandPos(frame, width, height)
+
+    # Debug
+    renderDebugView(rectId)
+
     if rectId is not None:
         if rectId != prevRectId:
             # prevRectId = rectId
             if rectId < len(view.screen.commands):
                 cmd, transition = view.screen.commands[rectId]
                 if transition:
-
+                    # Delay
                     if endTime is None:
                         currTime = time.time()
                         endTime = currTime + 0.5
@@ -94,30 +92,13 @@ while(camera.isOpened()):
                         print()
                         view.set_screen(transition)
                         for cmd, transition in view.screen.commands:
-                            print(cmd.action + " " + transition.name if transition else "None")
+                            print(cmd.action + " - " + transition.name if transition else "None")
                         endTime = None
                         prevRectId = rectId
- 
 
-    if rectId == 0:
-        cv2.putText(frame_out, "Top Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 1:
-        cv2.putText(frame_out, "Top Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 2:
-        cv2.putText(frame_out, "Bottom Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 3:
-        cv2.putText(frame_out, "Bottom Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    else:
-        cv2.putText(frame_out, "Nothing", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
- 
-    
-    cv2.imshow('Main', frame_out)
-
-    k = cv2.waitKey(10)
-    if k == 27:
+    key = cv2.waitKey(10)
+    if key == 27:
         camera.release()
+        view.destroy()
         cv2.destroyAllWindows()
         break
-    # elif k == ord('b'):
-        # bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
-        # isBgCaptured = 1
