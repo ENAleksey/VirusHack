8e9d872d2ba66c06af140cfa8111a6e381195567
ENAleksey <EN.Aleksey@gmail.com>
2020-05-04 16:26:11 +0500

    test navigation by hand_tracking

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,35 +1,11 @@
-# import cv2
-# cap=cv2.VideoCapture(0)
-# j=0
-# while 1:
-#     ret,frame=cap.read()
-#     if(j==0):
-#         bg=frame.copy().astype("float")
-#     if(j<30):
-#         cv2.accumulateWeighted(frame,bg,0.5)
-#         j=j+1
-#     diff=cv2.absdiff(frame,bg.astype("uint8"))
-#     diff=cv2.cvtColor(diff,cv2.COLOR_BGR2GRAY)
-#     thre,diff=cv2.threshold(diff,25,255,cv2.THRESH_BINARY)
-#     cv2.imshow("j",diff)
-#     if(cv2.waitKey(1) & 0XFF==ord('q')):
-#         break
-# cap.release()
-# cv2.destroyAllWindows()
-
 import cv2
 import numpy as np
 import math
 
 
-# camera = cv2.VideoCapture("http://192.168.100.12:8080/video")
-camera = cv2.VideoCapture(0)
-width = int(camera.get(3))
-height = int(camera.get(4))
-isBgCaptured = 0
-bgSubThreshold = 50
+
 learningRate = 0
-j=0
+
 
 
 def isHandInRect(frame, x, y, w, h):
@@ -72,15 +48,15 @@ def isHandInRect(frame, x, y, w, h):
 
 def getRectIdFromHandPos(frame, w, h):
     if isHandInRect(frame, 0, 0, 200, 200):
-        return 1
-    elif isHandInRect(frame, w-200, 0, 200, 200):
-        return 2
-    elif isHandInRect(frame, 0, h-200, 200, 200):
-        return 3
-    elif isHandInRect(frame, w-200, h-200, 200, 200):
-        return 4
-    else:
         return 0
+    elif isHandInRect(frame, w-200, 0, 200, 200):
+        return 1
+    elif isHandInRect(frame, 0, h-200, 200, 200):
+        return 2
+    elif isHandInRect(frame, w-200, h-200, 200, 200):
+        return 3
+    else:
+        return None
 
 
 def removeBG(frame):
@@ -92,54 +68,3 @@ def removeBG(frame):
     fgmask = cv2.erode(fgmask, kernel, iterations=1)
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
-
-
-while(camera.isOpened()):
-    ret, frame = camera.read()
-    frame = cv2.flip(frame, 1)
-    # frame = cv2.bilateralFilter(frame, 9, 350, 350)
-
-    # frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    # if(j==0):
-    #     bg=frame.copy().astype("float")
-    # if(j<30):
-    #     cv2.accumulateWeighted(frame,bg,0.5)
-    #     j=j+1
-    # frame=cv2.absdiff(frame,bg.astype("uint8"))
-    
-    # thre,frame=cv2.threshold(frame,25,255,cv2.THRESH_BINARY)
-
-    # if isBgCaptured == 1:
-        # frame = removeBG(frame)
-
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # frame = cv2.GaussianBlur(frame, (5, 5), 0)
-        # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-
-    rectId = getRectIdFromHandPos(frame, width, height)
-
-    if rectId == 1:
-        cv2.putText(frame,"Top Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 2:
-        cv2.putText(frame,"Top Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 3:
-        cv2.putText(frame,"Bottom Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    elif rectId == 4:
-        cv2.putText(frame,"Bottom Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-    else:
-        cv2.putText(frame,"Nothing", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
-
-    cv2.imshow('Main', frame)
-    cv2.rectangle(frame, (200,200), (000,000), (0,255,0), 0)
-    cv2.rectangle(frame, (width,height), (width-200,height-200), (0,255,0), 0)
-    cv2.rectangle(frame, (200,height), (0,height-200), (0,255,0), 0)
-    cv2.rectangle(frame, (width,200), (width-200,0), (0,255,0), 0)
-
-    k = cv2.waitKey(10)
-    if k == 27:
-        camera.release()
-        cv2.destroyAllWindows()
-        break
-    # elif k == ord('b'):
-        # bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
-        # isBgCaptured = 1
---
+++ test_hand.py
@@ -0,0 +1,101 @@
+from hand_tracking import getRectIdFromHandPos 
+import cv2
+import commands
+from view import View
+
+# camera = cv2.VideoCapture("http://192.168.100.12:8080/video")
+# camera = cv2.VideoCapture(0)
+# width = int(camera.get(3))
+# height = int(camera.get(4))
+view = View()
+
+# isBgCaptured = 0
+# bgSubThreshold = 50
+# j=0
+
+# while(camera.isOpened()):
+#     ret, frame = camera.read()
+#     frame = cv2.flip(frame, 1)
+#     # frame = cv2.bilateralFilter(frame, 9, 350, 350)
+
+#     # frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+#     # if(j==0):
+#     #     bg=frame.copy().astype("float")
+#     # if(j<30):
+#     #     cv2.accumulateWeighted(frame,bg,0.5)
+#     #     j=j+1
+#     # frame=cv2.absdiff(frame,bg.astype("uint8"))
+    
+#     # thre,frame=cv2.threshold(frame,25,255,cv2.THRESH_BINARY)
+
+#     # if isBgCaptured == 1:
+#         # frame = removeBG(frame)
+
+#         # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#         # frame = cv2.GaussianBlur(frame, (5, 5), 0)
+#         # _, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+#     rectId = getRectIdFromHandPos(frame, width, height)
+
+#     if rectId is not None:
+#         cmd, transition = view.screen.commands[rectId]
+#         print(cmd, transition)
+
+#     if rectId == 0:
+#         cv2.putText(frame,"Top Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+#     elif rectId == 1:
+#         cv2.putText(frame,"Top Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+#     elif rectId == 2:
+#         cv2.putText(frame,"Bottom Left", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+#     elif rectId == 3:
+#         cv2.putText(frame,"Bottom Right", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+#     else:
+#         cv2.putText(frame,"Nothing", (250, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 2, 2)
+
+#     cv2.imshow('Main', frame)
+#     cv2.rectangle(frame, (200,200), (000,000), (0,255,0), 0)
+#     cv2.rectangle(frame, (width,height), (width-200,height-200), (0,255,0), 0)
+#     cv2.rectangle(frame, (200,height), (0,height-200), (0,255,0), 0)
+#     cv2.rectangle(frame, (width,200), (width-200,0), (0,255,0), 0)
+
+#     k = cv2.waitKey(10)
+#     if k == 27:
+#         camera.release()
+#         cv2.destroyAllWindows()
+#         break
+#     # elif k == ord('b'):
+#         # bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+#         # isBgCaptured = 1
+
+
+
+# Начальный экран
+print(view.screen.name)
+for cmd, transition in view.screen.commands:
+    print(cmd.action, transition.name if transition else "None")
+
+# Добавление товаров
+rectId = 0
+cmd, transition = view.screen.commands[rectId]
+view.set_screen(transition)
+for cmd, transition in view.screen.commands:
+    print(cmd.action, transition.name if transition else "None")
+
+# Отмена покупки
+rectId = 1
+cmd, transition = view.screen.commands[rectId]
+view.set_screen(transition)
+for cmd, transition in view.screen.commands:
+    print(cmd.action, transition.name if transition else "None")
+
+exit(1)
+
+if rectId is not None:
+    if view.screen.commands[rectId] is not None:
+        cmd, transition = view.screen.commands[rectId]
+        print(cmd.action, transition.name)
+        view.set_screen(transition)
+
+        cmd, transition = view.screen.commands[1]
+        print(cmd.action, transition.name)
+        # view.set_screen(transition)
